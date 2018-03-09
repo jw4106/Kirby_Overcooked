5b3759b83adc98ad2994652d25dca0bd95718f68
Carlos Michael Rodriguez <cmr624@nyu.edu>
2018-03-09 14:31:13 -0500

    changed some formatting

--- sketch.js
+++ sketch.js
@@ -7,37 +7,76 @@ function preload(){
 	kirby_move = loadGif("assets/kirbymove.gif");
 	kirby_stop = loadImage("assets/kirbystop.png");
 	kirby_move_left = loadGif("assets/kirbymoveleft.gif");
-	kirby_stop_left = loadImage("assets/kirbystopleft.png");	
+	kirby_stop_left = loadImage("assets/kirbystopleft.png");
 }
 
 class Kirby{
-	constructor(){
+	constructor()
+	{
 		this.xPos = 400;
 		this.yPos = 300;
 		this.state = kirby_stop;
 	}
-	move(){
+	move()
+	{
+		if (keyIsDown(65) && keyIsDown(16) && this.xPos > 0)
+		{
+			this.xPos -= 4;
+			this.state = kirby_run_left;
+		}
+		else if (keyIsDown(65) && this.xPos > 0 )
+		{
+			this.xPos -= 2;
+			this.state = kirby_move_left;
+		}
 
-		if (keyIsDown(65) && keyIsDown(16) && this.xPos > 0 ) {this.xPos -= 4; this.state = kirby_run_left;}
-		else if (keyIsDown(65) && this.xPos > 0 ) {this.xPos -= 2; this.state = kirby_move_left;}
+		if (keyIsDown(68) && keyIsDown(16) && this.xPos < 750)
+		{
+			this.xPos += 4;
+			this.state = kirby_run;
+		}
+		else if (keyIsDown(68) && this.xPos < 750)
+		{
+			this.xPos += 2;
+			this.state = kirby_move;
+		}
 
-		if (keyIsDown(68) && keyIsDown(16) && this.xPos < 750) {this.xPos += 4; this.state = kirby_run;}
-		else if (keyIsDown(68) && this.xPos < 750) {this.xPos += 2; this.state = kirby_move;}
-		
-		if (keyIsDown(87) && keyIsDown(16) && this.yPos > 0)  {this.yPos -= 4; this.state = kirby_run;}
-		else if (keyIsDown(87) && this.yPos > 0)  {this.yPos -= 2; this.state = kirby_move;}
-		
-		if (keyIsDown(83) && keyIsDown(16) && this.yPos < 550) {this.yPos += 4; this.state = kirby_run;}
-		else if (keyIsDown(83) && this.yPos < 550) {this.yPos += 2; this.state = kirby_move;}
+		if (keyIsDown(87) && keyIsDown(16) && this.yPos > 0)
+		{
+			this.yPos -= 4;
+			this.state = kirby_run;
+		}
+		else if (keyIsDown(87) && this.yPos > 0)
+		{
+			this.yPos -= 2;
+			this.state = kirby_move;
+		}
 
+		if (keyIsDown(83) && keyIsDown(16) && this.yPos < 550)
+		{
+			this.yPos += 4;
+			this.state = kirby_run;
+		}
+		else if (keyIsDown(83) && this.yPos < 550)
+		{
+			this.yPos += 2;
+			this.state = kirby_move;
+		}
 
-
-		if (!keyIsDown(65) && !keyIsDown(68) && !keyIsDown(87) && !keyIsDown(83)) {
-			if(this.state === kirby_move){this.state = kirby_stop;}
-			if(this.state === kirby_move_left){this.state = kirby_stop_left;}
-		}		
+		if (!keyIsDown(65) && !keyIsDown(68) && !keyIsDown(87) && !keyIsDown(83))
+		{
+			if(this.state === kirby_move)
+				{
+					this.state = kirby_stop;
+				}
+			if(this.state === kirby_move_left)
+			{
+				this.state = kirby_stop_left;
+			}
+		}
 	}
-	display(){
+	display()
+	{
 		image(this.state, this.xPos, this.yPos, 50,50);
 	}
 }
