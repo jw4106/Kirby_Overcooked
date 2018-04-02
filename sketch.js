0c22ca5f8a1e4074a3b40d113a95fd115a72265e
jw4106 <jw4106@nyu.edu>
2018-04-02 09:54:34 -0400

    sounds

--- sketch.js
+++ sketch.js
@@ -4,13 +4,18 @@ let kirby, kirbyChef, kirbyChefObj;
 let fridgeObj, stoveObj, tableObj, order;
 let font, state, starty, bgMusic;
 
+//timer variable
 var counter=0;
+//length of cooking meat 
 var cooktime = 0;
 var timeleft=150;
+//plate object array
 var platehas = [];
 var score = 0;
+
 function preload()
 {
+	//load every single asset we have such as sound, gifs and images. 
 	kirby_run = loadGif("assets/kirbyrun.gif");
 	kirby_run_left = loadGif("assets/kirbyrunleft.gif");
 	kirby_move = loadGif("assets/kirbymove.gif");
@@ -18,26 +23,26 @@ function preload()
 	kirby_move_left = loadGif("assets/kirbymoveleft.gif");
 	kirby_stop_left = loadImage("assets/kirbystopleft.png");
 	map = loadImage("assets/map.jpg");
-  fridge = loadImage("assets/fridge.png");
-  knife = loadImage("assets/knife.png");
-  burger = loadImage("assets/burgerplate1.png");
-  cuttomato = loadImage("assets/slicedtomato.png");
-  cutcabbage = loadImage("assets/slicedcabbage.png")
-  cookedmeat = loadImage("assets/cooked_meat.png");
-  submittable = loadImage("assets/submit.png");
-  submitword = loadImage("assets/submitword.png");
-  meat = loadImage("assets/meat.png");
-  tableleft = loadImage("assets/table_left.png");
-  tablemiddle = loadImage("assets/table_middle.png");
-  smoke = loadGif("assets/smoke.gif");
-  tableright = loadImage("assets/table_right.png");
-  stove = loadImage("assets/stove.png");
-  tomato = loadImage("assets/tomato.png");
-  cabbage = loadImage("assets/veg.png");
-  pan = loadImage("assets/pan.png");
-  knife = loadImage("assets/knife.png");
-  sink = loadImage("assets/sink.png");
-  plate = loadImage("assets/plate.png");
+	fridge = loadImage("assets/fridge.png");
+	knife = loadImage("assets/knife.png");
+	burger = loadImage("assets/burgerplate1.png");
+	cuttomato = loadImage("assets/slicedtomato.png");
+	cutcabbage = loadImage("assets/slicedcabbage.png")
+	cookedmeat = loadImage("assets/cooked_meat.png");
+	submittable = loadImage("assets/submit.png");
+	submitword = loadImage("assets/submitword.png");
+	meat = loadImage("assets/meat.png");
+	tableleft = loadImage("assets/table_left.png");
+	tablemiddle = loadImage("assets/table_middle.png");
+	smoke = loadGif("assets/smoke.gif");
+	tableright = loadImage("assets/table_right.png");
+	stove = loadImage("assets/stove.png");
+	tomato = loadImage("assets/tomato.png");
+	cabbage = loadImage("assets/veg.png");
+	pan = loadImage("assets/pan.png");
+	knife = loadImage("assets/knife.png");
+	sink = loadImage("assets/sink.png");
+	plate = loadImage("assets/plate.png");
 	placeholder = loadImage("assets/swatch_light_gray.jpg");
 	kirbyChef = loadGif("assets/resized.gif");
 	order = loadImage("assets/order.png");
@@ -45,11 +50,14 @@ function preload()
 	starty = loadImage("assets/startscreen.png");
 	soundFormats("mp3", "m4a");
 	bgMusic = loadSound("sounds/newbgmusic.m4a");
+	cookMusic = loadSound("sounds/cooking.mp3");
+	submitMusic = loadSound("sounds/submitmusic.mp3");
 
 }
 
 class Kirby
 {
+	//holds the xposition, yposition and states of kirby for movement and holding objects 
 	constructor()
 	{
 		this.xPos = 400;
@@ -58,6 +66,7 @@ class Kirby
 		this.holding = false;
 		this.obj = null;
 	}
+	//movement logic for kirby
 	move()
 	{
 		//left, shift
@@ -153,11 +162,13 @@ class Kirby
 			}
 		}
 	}
+	//displays the object that kirby is holding 
 	display()
 	{
 		image(this.state, this.xPos, this.yPos, 50,50);
 		if (this.holding)
 		{
+			//if kirby is holding a plate, check if there is objects on the plate
 			if(this.obj === plate)
 			{
 				//place ingredients based on platehas array
@@ -174,6 +185,7 @@ class Kirby
 					}
 				}
 			}
+			//scaling for burger display
 			else if (this.obj === burger)
 			{
 				image(this.obj, this.xPos+10, this.yPos-25, 40, 40);
@@ -183,15 +195,18 @@ class Kirby
 			}
 		}
 	}
+	//change kirby state if he holds something
 	pickUp(obj)
 	{
 		this.holding = true;
 		this.obj = obj;
 	}
+	//change state if drop
 	drop(obj)
 	{
 		this.holding = false;
 	}
+	//function to return what kirby is holding
 	place(obj)
 	{
 		//place the object on the table.
@@ -200,8 +215,10 @@ class Kirby
 	}
 }
 
+//interactable objects that hold items and can place items/make special actions
 class Interactive
 {
+	//states for objects
 	constructor(sprite, x, y, hasObject, obj, acceptedObj)
 	{
 		this.sprite = sprite;
@@ -224,16 +241,6 @@ class Interactive
 		let kirbyLeft  = kirby.xPos;
 		let kirbyTop    = kirby.yPos;
 		let kirbyBottom = kirby.yPos + 50;
-		// if(this.sprite === stove || this.sprite === fridge || this.sprite === sink){
-		// 	spriteBottom = this.y + this.sprite.height;
-		// 	spriteLeft = 0;
-		// 	spriteRight = 800;
-		// }
-		// else{
-		//     spriteLeft  = this.x;
-		//     spriteRight = this.x + this.sprite.width;
-		// 	spriteBottom = this.y + this.sprite.height;
-		// }
 
 		if (spriteRight < kirbyLeft || spriteLeft > kirbyRight || spriteBottom < kirbyTop || spriteTop > kirbyBottom)
 		{
@@ -348,17 +355,22 @@ class Interactive
 			}
 		}
 	}
+	//display ingredients/tools on top of interactable object
 	display()
 	{
+		//display object
 		image(this.sprite, this.x, this.y);
+		//if there is an object
 		if (this.hasObject)
 		{
+			//change burger to plate upon successful submission
 			if(this.sprite === submittable && this.obj === burger){
 					image(plate, this.x, this.y, 30, 30);
 					this.obj = plate;
 					score += 50;
+					submitMusic.play()
 			}
-			//if object is table, and holds a plate....
+			//if object is table, and holds a plate display all items on plate as well
 			else if((this.sprite === table || this.sprite === tablemiddle || this.sprite === tableleft || this.sprite === tableright) && this.obj === plate)
 			{
 				if (this.obj === burger)
@@ -384,6 +396,7 @@ class Interactive
 			}
 			else
 			{
+				//scaling display for burger 
 				if (this.obj === burger)
 				{
 					image(this.obj, this.x-5, this.y-7, 40, 40);
@@ -397,23 +410,17 @@ class Interactive
 	}
 	action()
 	{
+		//if cooking...cook and play cooking music
 		if (this.sprite === stove && this.obj === meat)
 		{
 			let counter = 0;
+			cookMusic.play();
 			this.cook(counter);
 		}
 		if ((this.sprite === table || this.sprite === tablemiddle || this.sprite === tableright || this.sprite === tableleft) && this.obj === meat)
 		{
 			let counter = 0;
 			this.cook(counter);
-		}
-		//else if (this.sprite === tableWithKnife && (this.obj.sprite === cabbage || this.obj.sprite === tomato))
-		//{
-		//	this.chop();
-		//}
-		if (this.sprite === submittable && this.obj === burgerplate1)
-		{
-
 		}
 	}
 	cook(counter)
@@ -422,23 +429,16 @@ class Interactive
 		if(cooktime === 300){
 			this.obj = cookedmeat;
 			cooktime = 0;
+			cookMusic.stop();
 		}
 		else{
 			image(smoke, 405, 35, 60, 60);
 			cooktime++;
 		}
 	}
-	checkOrder()
-	{
-		//console.log("checkOrder");
-	}
-		//if (this.obj)
 }
 
-
-
 //Timer: convert seconds to min
-
 function convertSeconds(s)
 {
     var min = floor (s/60);
@@ -450,40 +450,38 @@ function convertSeconds(s)
 function setup()
 {
 	createCanvas(800,600);
-  kirby = new Kirby();
+	kirby = new Kirby();
+	//initialize everything! 
+	fridgeObj = new Interactive(fridge,500,50, false, null, null);
 
-	kirbyChefObj = new Interactive(kirbyChef, 600, 50, false, null, true);
+	submission = new Interactive(submittable, 650, 110, false, null, true);
+	submitwords = new Interactive(submitword, 650,50,false,null,false);
 
-  fridgeObj = new Interactive(fridge,500,50, false, null, null);
+	tableObj_left = new Interactive(tableleft, 590, 500, true, meat, true);
+	tableObj_middle = new Interactive(tablemiddle, 630, 500, true, meat, true);
+	tableObj_middle2 = new Interactive(tablemiddle, 665, 500, true, meat, true);
+	tableObj_right = new Interactive(tableright, 700, 500, true, meat, true);
 
-  submission = new Interactive(submittable, 650, 110, false, null, true);
-  submitwords = new Interactive(submitword, 650,50,false,null,false);
+	tableObj2_left = new Interactive(tableleft, 590, 310, false, null, true);
+	tableObj2_middle = new Interactive(tablemiddle, 630, 310, true, plate, true);
+	tableObj2_middle2 = new Interactive(tablemiddle, 665, 310, false, null, true);
+	tableObj2_right = new Interactive(tableright, 700, 310, true, meat, true);
 
-  tableObj_left = new Interactive(tableleft, 590, 500, true, meat, true);
-  tableObj_middle = new Interactive(tablemiddle, 630, 500, true, meat, true);
-  tableObj_middle2 = new Interactive(tablemiddle, 665, 500, true, meat, true);
-  tableObj_right = new Interactive(tableright, 700, 500, true, meat, true);
+	tableObj3_left = new Interactive(tableleft, 340, 500, false, null, true);
+	tableObj3_middle = new Interactive(tablemiddle, 380, 500, true, tomato, true);
+	tableObj3_middle2 = new Interactive(tablemiddle, 415, 500, true, knife, true);
+	tableObj3_right = new Interactive(tableright, 450, 500, true, cabbage, true);
 
-  tableObj2_left = new Interactive(tableleft, 590, 310, false, null, true);
-  tableObj2_middle = new Interactive(tablemiddle, 630, 310, true, plate, true);
-  tableObj2_middle2 = new Interactive(tablemiddle, 665, 310, false, null, true);
-  tableObj2_right = new Interactive(tableright, 700, 310, true, meat, true);
-
-  tableObj3_left = new Interactive(tableleft, 340, 500, false, null, true);
-  tableObj3_middle = new Interactive(tablemiddle, 380, 500, true, tomato, true);
-  tableObj3_middle2 = new Interactive(tablemiddle, 415, 500, true, knife, true);
-  tableObj3_right = new Interactive(tableright, 450, 500, true, cabbage, true);
-
-  stoveObj = new Interactive(stove,400,50, false, null, meat);
-  sinkObj = new Interactive(sink,250,50, false, null, true);
-  tomatoObj1 = new Interactive(placeholder,100,320,true, tomato, true);
-  tomatoObj2 = new Interactive(placeholder,155,320,true, tomato, true);
-  tomatoObj3 = new Interactive(placeholder,155,380,true, tomato, true);
-  tomatoObj4 = new Interactive(placeholder,100,380,true, tomato, true);
-  cabbageObj1 = new Interactive(placeholder,100,450,true, cabbage, true);
-  cabbageObj2 = new Interactive(placeholder,155,450,true, cabbage, true);
-  cabbageObj3 = new Interactive(placeholder,155,510,true, cabbage, true);
-  cabbageObj4 = new Interactive(placeholder,100,510,true, cabbage, true);
+	stoveObj = new Interactive(stove,400,50, false, null, meat);
+	sinkObj = new Interactive(sink,250,50, false, null, true);
+	tomatoObj1 = new Interactive(placeholder,100,320,true, tomato, true);
+	tomatoObj2 = new Interactive(placeholder,155,320,true, tomato, true);
+	tomatoObj3 = new Interactive(placeholder,155,380,true, tomato, true);
+	tomatoObj4 = new Interactive(placeholder,100,380,true, tomato, true);
+	cabbageObj1 = new Interactive(placeholder,100,450,true, cabbage, true);
+	cabbageObj2 = new Interactive(placeholder,155,450,true, cabbage, true);
+	cabbageObj3 = new Interactive(placeholder,155,510,true, cabbage, true);
+	cabbageObj4 = new Interactive(placeholder,100,510,true, cabbage, true);
 	bgMusic.play();
 	state = 0;
 	//timer count down
@@ -509,13 +507,12 @@ function draw()
 			counter = 0;
 		}
 	}
+	//gamestart
 	else if (state === 1)
 	{
+		//display and set collision
 		background(map);
 		kirby.move();
-		kirbyChefObj.display();
-		kirbyChefObj.check(kirby);
-		kirbyChefObj.checkOrder();
 
 		fridgeObj.display();
 		fridgeObj.check(kirby);
@@ -551,38 +548,40 @@ function draw()
 		stoveObj.display();
 		stoveObj.check(kirby);
 		stoveObj.action();
-	  sinkObj.display();
+		sinkObj.display();
 		sinkObj.check(kirby);
-	  tomato.resize(30, 38);
-	  tomatoObj1.display();
+		tomato.resize(30, 38);
+		tomatoObj1.display();
 		tomatoObj1.check(kirby);
-	  tomatoObj2.display();
+		tomatoObj2.display();
 		tomatoObj2.check(kirby);
-	  tomatoObj3.display();
+		tomatoObj3.display();
 		tomatoObj3.check(kirby);
-	  tomatoObj4.display();
+		tomatoObj4.display();
 		tomatoObj4.check(kirby);
-	  cabbage.resize(30, 38);
-	  cabbageObj1.display();
+		cabbage.resize(30, 38);
+		cabbageObj1.display();
 		cabbageObj1.check(kirby);
-	  cabbageObj2.display();
+		cabbageObj2.display();
 		cabbageObj2.check(kirby);
-	  cabbageObj3.display();
+		cabbageObj3.display();
 		cabbageObj3.check(kirby);
-	  cabbageObj4.display();
+		cabbageObj4.display();
 		cabbageObj4.check(kirby);
 		kirby.display();
+		//User Interface
 		textFont(font, 14);
 		text("Kirby's Burger Recipe:", 10, 60)
 		image(order, 165, 10, 90, 90);
 		textFont(font, 25);
-	  text("Time left: " + convertSeconds(timeleft-counter), 300, 50);
-	  text("Score: " + score, 300, 25);
+		text("Time left: " + convertSeconds(timeleft-counter), 300, 50);
+		text("Score: " + score, 300, 25);
 		if (timeleft-counter < 0)
 		{
 			state = 2;
 		}
 	}
+	//game over screen
 	else if (state === 2)
 	{
 		background(starty);
